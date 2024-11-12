804b34f1a305f69a8c5653718922d61346267ea9
tekweb_2024_2300016067 <2300016067@webmail.uad.ac.id>
2024-11-12 21:26:22 +0700

    uts coba

---
+++ script.js
@@ -0,0 +1,25 @@
+const generatePassword = (length, options) => {
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercase = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const specialChars = "!@#$%^&*()";
+
+    let charset = '';
+    if (options.includeUppercase) charset += uppercase;
+    if (options.includeLowercase) charset += lowercase;
+    if (options.includeNumbers) charset += numbers;
+    if (options.includeSpecialChars) charset += specialChars;
+
+    if (charset.length === 0) {
+        throw new Error("At least one character type must be selected.");
+    }
+
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+};
+
+module.exports = { generatePassword };
